8623c984d1a46b90cbddb73ee0cd15100826c3a6
NightFury0109 <nightfury0109@gmail.com>
2022-05-30 15:57:56 +0200

    add the server for p2p

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const express = require("express");
-const http = require("http");
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+const p2p = require('socket.io-p2p-server').Server;
 
-const app = express();
-
-const port = process.env.PORT || 9000;
-
-//initialize a http server
-const server = http.createServer(app);
-
-//start our server
-server.listen(port, () => {
-    console.log(`Server started on port ${server.address().port} :)`);
+server.listen(9000, () => {
+    console.log(`Server started on port 9000 :)`);
 });
+
+io.use(p2p);
+
+io.on('connection', function (socket) {
+    socket.on('peer-msg', function (data) {
+        console.log('Message from peer: %s', data)
+        socket.broadcast.emit('peer-msg', data)
+    })
+
+    socket.on('peer-file', function (data) {
+        console.log('File from peer: %s', data)
+        socket.broadcast.emit('peer-file', data)
+    })
+
+    socket.on('go-private', function (data) {
+        socket.broadcast.emit('go-private', data)
+    })
+})
