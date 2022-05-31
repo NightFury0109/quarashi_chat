e9b354f94b4f002a837eef7d19a8216e135c4d3b
NightFury0109 <nightfury0109@gmail.com>
2022-05-31 08:42:37 +0200

    remove the socket.io-p2p module

--- client/src/api/message/message.js
+++ client/src/api/message/message.js
@@ -1,18 +1,24 @@
-import api from './../../utils/api'
-import P2P from 'socket.io-p2p'
-import io from 'socket.io-client'
+// import api from './../../utils/api'
+import P2P from 'socket.io-p2p';
+import io from 'socket.io-client';
+const socket = io.connect('http://localhost:5000');
 
-let socket = io();
-let p2p = new P2P(socket)
-p2p.useSockets = false;
-p2p.emit('go-private', true)
+let opts = { peerOpts: { trickle: false }, autoUpgrade: false }
+
+const p2p = new P2P(socket, null, () => {
+    console.log('my ID is: ', p2p.peerId)
+});
+
+// p2p.usePeerConnection = true;
 
 p2p.on('peer-msg', function (data) {
     console.log('From a peer %s', data);
 });
 
 export const send_message = (message) => {
-    if(typeof window !== "undefined"){
-        p2p.emit('peer-msg', { textVal: message })
-    }
+    console.log(message)
+    // p2p.emit('peer-msg', { message: message })
+    p2p.on('peer-msg', function (data) {
+        console.log('From a peer %s', data);
+    });
 }
