95f4283ed15b1f5d50abbe016b05a3e0bf8318b0
NightFury0109 <nightfury0109@gmail.com>
2022-05-31 08:14:40 +0200

    change the port with 5000

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server);
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
+});
 const p2p = require('socket.io-p2p-server').Server;
 
-server.listen(9000, () => {
-    console.log(`Server started on port 9000 :)`);
+server.listen(5000, () => {
+    console.log(`Server started on port 5000 :)`);
 });
 
 io.use(p2p);
 
 io.on('connection', function (socket) {
     socket.on('peer-msg', function (data) {
-        console.log('Message from peer: %s', data)
-        socket.broadcast.emit('peer-msg', data)
+        console.log('Message from peer: %s', data.message)
     })
 
     socket.on('peer-file', function (data) {
         console.log('File from peer: %s', data)
-        socket.broadcast.emit('peer-file', data)
     })
 
     socket.on('go-private', function (data) {
