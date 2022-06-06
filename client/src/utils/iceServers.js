b0bf6508cf25a143646214b700277bccca8a3368
NightFury0109 <nightfury0109@gmail.com>
2022-06-06 20:01:55 +0200

    trying with own siginaling server

--- client/src/utils/iceServers.js
+++ client/src/utils/iceServers.js
@@ -3,4 +3,8 @@ export const iceServers =
         urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
         username: 'webrtc',
         credential: 'webrtc',
-    }] ;
+    }];
+
+export const ownIceServer = [{
+    urls: ''
+}]
