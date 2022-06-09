5ec502bd48d4ace3bf34cd73c73fbf5cb607325b
NightFury0109 <nightfury0109@gmail.com>
2022-06-09 09:28:34 +0200

    tying with ubuntu 22.04 server

--- client/src/utils/iceServers.js
+++ client/src/utils/iceServers.js
@@ -6,7 +6,8 @@ export const iceServers =
     }];
 
 export const ownIceServer = [{
-    urls: 'http://172.31.27.238',
+    // urls: 'http://172.31.27.238',
+    urls: '178.128.192.242',
     username: 'jovan',
     credential: '123456'
 }]
