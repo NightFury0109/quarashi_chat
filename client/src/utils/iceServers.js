df03290eba7dbe54e92bbc05fcbb712af1085a93
NightFury0109 <nightfury0109@gmail.com>
2022-06-11 04:31:25 +0200

    change the iceserver

--- client/src/utils/iceServers.js
+++ client/src/utils/iceServers.js
@@ -1,13 +1,26 @@
-export const iceServers =
-    [{
+// export const iceServers = [
+//     // Test some STUN server
+//     {
+//         urls: 'stun:178.128.192.242?transport=udp',
+//     },
+//     // Test some TURN server
+//     {
+//         urls: 'turn:178.128.192.242?transport=tcp',
+//         username: 'webrtc',
+//         credential: 'webrtc',
+//     }
+// ];
+
+export const iceServers = [
+    {
+        urls: 'stun:stun.anyfirewall.com:443?transport=tcp'
+    },
+    {
         urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
         username: 'webrtc',
         credential: 'webrtc',
-    }];
+    }
+]
 
-export const ownIceServer = [{
-    // urls: 'http://172.31.27.238',
-    // urls: '178.128.192.242',
-    // username: 'jovan',
-    // credential: '123456'
-}]
+// urls: 'http://172.31.27.238',
+
