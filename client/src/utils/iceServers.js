acf50d2aa90b04d965d6d448491ef3832c0bac94
NightFury0109 <nightfury0109@gmail.com>
2022-06-13 19:34:46 +0000

    check the iceserver with ubuntu 22.04, and it works

--- client/src/utils/iceServers.js
+++ client/src/utils/iceServers.js
@@ -1,28 +1,30 @@
-// export const iceServers = [
-//     // Test some STUN server
-//     {
-//         urls: 'stun:178.128.192.242?transport=udp',
-//     },
-//     // Test some TURN server
-//     {
-//         urls: 'turn:178.128.192.242?transport=tcp',
-//         username: 'webrtc',
-//         credential: 'webrtc',
-//     }
-// ];
-
 export const iceServers = {
     'iceServers': [
+        // Test some STUN server
         {
-            urls: 'stun:stun.anyfirewall.com:443?transport=tcp'
+            urls: 'stun:178.128.192.242?transport=udp',
         },
+        // Test some TURN server
         {
-            urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
+            urls: 'turn:178.128.192.242?transport=udp',
             username: 'webrtc',
             credential: 'webrtc',
         }
     ]
-}
+};
+
+// export const iceServers = {
+//     'iceServers': [
+//         {
+//             urls: 'stun:stun.anyfirewall.com:443?transport=tcp'
+//         },
+//         {
+//             urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
+//             username: 'webrtc',
+//             credential: 'webrtc',
+//         }
+//     ]
+// }
 
 // urls: 'http://172.31.27.238',
 
