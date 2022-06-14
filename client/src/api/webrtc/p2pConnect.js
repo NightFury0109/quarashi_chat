a90bc2ac2460f6b487b4d27795372de30e2359b5
NightFury0109 <nightfury0109@gmail.com>
2022-06-14 06:20:59 +0000

    delete turn and stun connect

--- client/src/api/webrtc/p2pConnect.js
+++ client/src/api/webrtc/p2pConnect.js
@@ -1,3 +1,19 @@
 export const p2pConnect = () => {
     console.log('p2pConnect')
 }
+
+const createPeerConnectionSturn = async () => {
+    const signalingChannel = new SignalingChannel(remoteClientId);
+    const configuration = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] }
+    const peerConnection = new RTCPeerConnection(configuration);
+    signalingChannel.addEventListener('message', async message => {
+        if (message.answer) {
+            const remoteDesc = new RTCSessionDescription(message.answer);
+            await peerConnection.setRemoteDescription(remoteDesc);
+            console.log(remoteDesc)
+        }
+    });
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    signalingChannel.send({ 'offer': offer });
+}
