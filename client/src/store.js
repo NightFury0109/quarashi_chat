ace03d6ef7fcc36f9f05e01796e2c5113cc29fd9
NightFury0109 <nightfury0109@gmail.com>
2022-06-16 17:16:45 +0200

    add switch and save in store

--- client/src/store.js
+++ client/src/store.js
@@ -2,3 +2,6 @@ import { writable } from 'svelte/store';
 
 export const connectionSecure = writable({});
 export const socketStore = writable({});
+
+export const privateSetting = writable("");
+export const notificationSetting = writable("");
